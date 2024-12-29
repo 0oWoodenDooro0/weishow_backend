c46cf2bc8abd181a1bf0ac99c624bc721bc40d3a
0oWoodenDooro0 <vincent031525@gmail.com>
2024-12-30 01:36:26 +0800

    many

--- src/main/kotlin/com/gmail/vincent031525/domain/model/ManagerDto.kt
+++ src/main/kotlin/com/gmail/vincent031525/domain/model/ManagerDto.kt
@@ -1,5 +1,8 @@
 package com.gmail.vincent031525.domain.model
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class ManagerDto(
     val id: Int? = null,
     val username: String,
